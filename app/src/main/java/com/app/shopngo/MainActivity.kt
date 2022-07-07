c7f6a72da74fd240d6ede3b0901c50342afb89f7
GeraldDZ <dzulfiqarg@gmail.com>
2022-07-07 22:38:33 +0700

    add Action Bar, Bottom Navigation, & Fragment

--- app/src/main/java/com/app/shopngo/MainActivity.kt
+++ app/src/main/java/com/app/shopngo/MainActivity.kt
@@ -1,41 +1,64 @@
 package com.app.shopngo
 
+import android.app.AlertDialog
+import android.content.DialogInterface
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
+import android.view.Menu
+import android.view.MenuItem
 import android.widget.TextView
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+import androidx.fragment.app.Fragment
 import com.google.android.gms.auth.api.signin.GoogleSignIn
 import com.google.android.gms.auth.api.signin.GoogleSignInClient
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.google.firebase.auth.FirebaseAuth
 
 class MainActivity : AppCompatActivity() {
     private lateinit var auth: FirebaseAuth
     private lateinit var tvemail : TextView
-    private lateinit var btnSignOut : Button
+    private lateinit var toolbar: Toolbar
+    private lateinit var bottomnav : BottomNavigationView
     private lateinit var mGoogleSignInClient: GoogleSignInClient
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        toolbar = findViewById(R.id.tbout)
+        bottomnav = findViewById(R.id.bottomnav)
         tvemail = findViewById(R.id.tvEmail)
-        btnSignOut = findViewById(R.id.btnSignOut)
+
+        setSupportActionBar(toolbar)
+        supportActionBar?.setDisplayShowTitleEnabled(false)
+
         initFirebase()
         getData()
 
-        btnSignOut.setOnClickListener {
-            auth.signOut()
-            mGoogleSignInClient.signOut()
-            Toast.makeText(this, "Sign Out Berhasil", Toast.LENGTH_SHORT).show()
-            startActivity(Intent(this, SignIn::class.java))
-            finish()
-        }
+        replacefragment(HomeFragment())
 
+        bottomnav.setOnItemSelectedListener {
+            when (it.itemId) {
+                R.id.home -> replacefragment(HomeFragment())
+                R.id.cart -> replacefragment(CartFragment())
+                R.id.scanner -> replacefragment(ScannerFragment())
+                R.id.history -> replacefragment(HistoryFragment())
+                R.id.setting -> replacefragment(SettingFragment())
+
+            }
+            true
+        }
     }
 
+    private fun replacefragment(fragment : Fragment){
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.fragment_container, fragment)
+        fragmentTransaction.commit()
+    }
     private fun initFirebase() {
         auth = FirebaseAuth.getInstance()
         val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
@@ -49,7 +72,32 @@ class MainActivity : AppCompatActivity() {
     private fun getData() {
         val user = auth.currentUser
         if (user != null){
-            tvemail.text = user.email
+            tvemail.text = user.displayName.toString()
         }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.toolbarmenu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId){
+            R.id.btnSignOut->{
+                val builder = AlertDialog.Builder(this)
+                builder.setTitle("Peringatan !!! ")
+                    .setMessage("Apakah Anda Ingin Keluar ? ")
+                    .setPositiveButton("Yes") { dialog: DialogInterface?, which: Int ->
+                        auth.signOut()
+                        mGoogleSignInClient.signOut()
+                        Toast.makeText(this, "Sign Out Berhasil", Toast.LENGTH_SHORT).show()
+                        startActivity(Intent(this, SignIn::class.java))
+                        finish()
+                    }.setNegativeButton(
+                        "No"
+                    ) { dialog: DialogInterface, which: Int -> dialog.cancel() }.show()
+            }
+        }
+        return true
     }
 }
